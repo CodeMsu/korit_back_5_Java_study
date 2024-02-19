15d20031000f988fee046c3816de103fdab79477
dawdawhhsh <ddukbai98@gmail.com>
2024-02-19 17:35:32 +0900

    java_study

---
+++ korit_back_5_Java_study/src/main/java/com/study/ch06/For03.java
@@ -0,0 +1,96 @@
+package com.study.ch06;
+
+import java.util.Scanner;
+
+public class For03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("별 몇개? ");
+        int starCount = scanner.nextInt();
+
+        for(int i = 0; i < starCount; i++) {
+            for(int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < starCount; i++) {
+            for(int j = 0; j < starCount - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        /*
+
+          *
+         **
+        ***
+
+         */
+        for(int i = 0; i < starCount; i++) {
+            for(int j = 0; j < starCount - 1 - i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < starCount; i++) {
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < starCount - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < starCount; i++) {
+            for(int j = 0; j < starCount - 1 - i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < (i + 1) * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        /*
+
+        *
+        **
+        ***
+
+
+        ***
+        **
+        *
+
+          *
+         **
+        ***
+
+        ***
+         **
+          *
+
+          *
+         ***
+        *****
+
+
+         */
+    }
+}
